886920bb8b06ac2a23a22d450603a018748c2c39
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-12-18 22:15:13 +0000

    libdex: Invert the default for dexopt-on-cache
    The growth of /system apps and total /data space are making this
    device-breaking (insufficient space to dexopt everything to
    /cache), pointless (/data has more than enough room to hold
    the cache), and in some cases, both.
    The default behavior is now the same as mainline Android's: dexopt
    everything to /data. Set dalvik.vm.dexopt-data-only=0 to change
    that and dexopt /system apps to /cache. If you do that, make sure
    your /cache can accomodate double the size of the dex classes in
    system, since in some extreme cases that has been observed during
    dex2oat
    
    Change-Id: Ia99655731c882e54bdc85293c467288fb4dc65df

--- libdex/OptInvocation.cpp
+++ libdex/OptInvocation.cpp
@@ -96,7 +96,26 @@ char* dexOptGenerateCacheFileName(const char* fileName, const char* subFileName)
     dataRoot = getenv("ANDROID_DATA");
     if (dataRoot == NULL)
         dataRoot = "/data";
-    snprintf(nameBuf, kBufLen, "%s/%s", dataRoot, kCacheDirectoryName);
+
+    if (systemRoot == NULL)
+        systemRoot = "/system";
+
+#ifdef ALLOW_DEXROOT_ON_CACHE
+    if (dexRoot == NULL)
+#endif
+        dexRoot = dataRoot;
+
+#ifdef ALLOW_DEXROOT_ON_CACHE
+    /* Cache anything stored on /system in cacheRoot, everything else in dataRoot */
+    if (!strncmp(absoluteFile, systemRoot, strlen(systemRoot))) {
+        property_get("dalvik.vm.dexopt-data-only", dexoptDataOnly, "1");
+        if (strcmp(dexoptDataOnly, "1") != 0) {
+            dexRoot = cacheRoot;
+        }
+    }
+#endif
+
+    snprintf(nameBuf, kBufLen, "%s/%s", dexRoot, kCacheDirectoryName);
 
     /* Tack on the file name for the actual cache file path.
      */
